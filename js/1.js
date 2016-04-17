4d44cfa55268033c631d1db891ffdfdbf4518091
yambee <yambee-star@mail.ru>
2016-04-17 06:24:37 +0300

    начали с момента подсчета кол-ва каждой из букв входящих в строку
    не считает почему-то

---
+++ js/1.js
@@ -0,0 +1,37 @@
+a = [];  //пустой массивчик
+
+for (i=0; i<5; i++) {
+	var b = {
+				name: 'susan',
+				gender: 'fem '+i
+			};
+	b[i] = i;
+	a.push(b);
+}
+
+console.log(a);
+
+
+// вывести календарь
+// список
+
+// посчитать кол-во букв в слове
+
+st = prompt ("Введите слово");
+
+console.log (st);
+console.log (typeof st);
+console.log (st.length);
+
+// посчитать кол-во слов в абзаце (считаем кол0во пробелов и выдаём + 1)
+
+for (i=0,j=0;i<st.length;i++){if (st[i]===" ") {j++}}
+console.log(j+1);
+
+// посчитать кол-во каждой из буквы, присутсвующих в строке
+
+for (i=0, j={}; i<st.length; i++) {
+	l = j[st[i]];
+	if (l) {l++} else {l=1}	
+};
+console.log(j);
